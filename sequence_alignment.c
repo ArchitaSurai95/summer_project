e787398d5c721cb81c498431ed0dca47afaf716b
ArchitaSurai95 <archita.surai@gmail.com>
2016-06-10 19:22:58 +0530

    sequence alignment done serially

--- sequence_alignment.c
+++ sequence_alignment.c
@@ -2,9 +2,9 @@
 #include<time.h>
 #include<string.h>
 #include<limits.h>
-#define MATCH 1
-#define MISMATCH 0
-#define GAP 0
+#define MATCH 2
+#define MISMATCH -3
+#define GAP -2
 int max(int var1,int var2,int var3)
 {
 	if(var1>var2)
@@ -22,10 +22,24 @@ int max(int var1,int var2,int var3)
 			return var3;
 	}
 }
+void string_rev(char str[])
+{
+	int i=0;
+	int j=strlen(str)-1;
+	char temp;
+	while (i < j)
+	 {
+	      temp = str[i];
+	      str[i] = str[j];
+	      str[j] = temp;
+	      i++;
+	      j--;
+	}
+}
 int main()
 {
-	char str1[100],str2[100];
-	int len1,len2,row_counter,col_counter;	
+	char str1[100],str2[100],align1[100],align2[100];
+	int len1,len2,row_counter,col_counter,index1,index2;;	
 	printf("Enter sequence 1 and sequence 2:\n");
 	scanf("%s %s",str1,str2);
 	len1=strlen(str1);
@@ -34,6 +48,12 @@ int main()
 	for(row_counter=0;row_counter<=len1;row_counter++)
 	for(col_counter=0;col_counter<=len2;col_counter++)
 	dp[row_counter][col_counter]=0;
+
+	for(row_counter=1;row_counter<=len1;row_counter++)
+	dp[row_counter][0]=dp[row_counter-1][0]+GAP;
+
+	for(col_counter=1;col_counter<=len2;col_counter++)
+	dp[0][col_counter]=dp[0][col_counter-1]+GAP;
 
 	for(row_counter=1;row_counter<=len1;row_counter++)
 	for(col_counter=1;col_counter<=len2;col_counter++)
@@ -51,5 +71,47 @@ int main()
 			printf("%d ",dp[row_counter][col_counter]);
 		printf("\n");
 	}
+
+	row_counter=len1;
+	col_counter=len2;
+	index1=0;
+	index2=0;
+	while(row_counter!=0||col_counter!=0)
+	{
+		if((dp[row_counter-1][col_counter-1]==dp[row_counter][col_counter-1])
+		&&(dp[row_counter-1][col_counter-1]==dp[row_counter-1][col_counter]))
+		{
+			align1[index1++]=str1[row_counter-1];
+			align2[index2++]=str2[col_counter-1];
+			row_counter--;
+			col_counter--;
+		}
+		else if((dp[row_counter-1][col_counter-1]>dp[row_counter][col_counter-1])
+			&&(dp[row_counter-1][col_counter-1]>dp[row_counter-1][col_counter]))
+			{
+				align1[index1++]=str1[row_counter-1];
+				align2[index2++]=str2[col_counter-1];
+				row_counter--;
+				col_counter--;
+			}
+		else if(dp[row_counter-1][col_counter]>dp[row_counter-1][col_counter-1])
+			{
+				align2[index2++]='$';
+				align1[index1++]=str1[row_counter-1];
+				row_counter--;
+			}		
+		else
+		{
+			align1[index1++]='$';
+			align2[index2++]=str2[col_counter-1];
+			col_counter--;
+		}
+	}
+	align1[index1]='\0';
+	align2[index2]='\0';
+	string_rev(align1);
+	string_rev(align2);
+	printf("The aligned strings are :\n%s\n%s\n",align1,align2);
+	
 	return 0;
 }
